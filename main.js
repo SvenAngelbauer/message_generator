3d58293b39ba380de02e0d6f93d6a539c26548da
Sven Angelbauer <sven.angelbauer@pm.me>
2021-02-08 14:37:51 +0000

    Updated jokeStore object to include error checking

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // CodeCademy Portfolio Porject :: Message Generator
 // https://www.codecademy.com/paths/full-stack-engineer-career-path/tracks/fscp-javascript-syntax-portfolio-project/modules/fscp-mixed-messages/kanban_projects/mixed-messages
+// Jokes from https://inews.co.uk/light-relief/jokes/knock-knock-jokes-funny-best-100-189897
 
 /**
  * Object which stores all jokes to be returned by the message generator.
@@ -11,9 +12,23 @@ const jokeStore = {
   // from which a knock knock joke can be compiled.
    _jokeArr: [],
 
-  // method to add a joke in the form ['Article', 'Punchline'].
+  // method to add a joke in the form ['Article', 'Punchline']. Will only add a
+  // new joke i.e. one which isn't already held in the this._jokeArr array.
    addJoke(article, punchLine) {
-    this._jokeArr.push([article, punchLine]);
+    let contains = false;
+
+    for (let i = 0; i < this._jokeArr.length; i++) {
+      if (this._jokeArr[i][0] === article && this._jokeArr[i][1] === punchLine) {
+        contains = true;
+        break;
+      }  
+    }
+
+    if (contains) {
+      console.log(`Joke "${[article, punchLine]}" has already been added.`);
+    } else {
+      this._jokeArr.push([article, punchLine]);
+    }
   },
 
   // method to remove a joke at a specific index. Will print a message if an
@@ -23,9 +38,9 @@ const jokeStore = {
       this._jokeArr.splice(ind, 1);
     } else if (this._jokeArr.length <= ind) {
       if (this._jokeArr.length === 1)       {
-        console.log(`There is no element at index ${ind}. There is only ${this._jokeArr.length} joke saved.`);
+        console.log(`There is no element ${ind + 1}. There is only ${this._jokeArr.length} joke saved.`);
       } else {
-        console.log(`There is no element at index ${ind}. There are only ${this._jokeArr.length} jokes saved.`);
+        console.log(`There is no element ${ind + 1}. There are only ${this._jokeArr.length} jokes saved.`);
       }
     } else {
       console.log('The array is already empty');
@@ -50,7 +65,8 @@ const jokeStore = {
 
 // testing code below - to be deleted for production
 
-jokeStore.addJoke('Stopwatch','Stopwatcha doin’ and open the stupid door.');
+jokeStore.addJoke('Stopwatch.','Stopwatcha doin’ and open the stupid door.');
+
 //console.log(jokeStore);
 //console.log(jokeStore.jokeArr);
 console.log('');
